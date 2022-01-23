cb39facf627f60ace05e94129c24b77bfbf6b09c
Ian Li <ian.li1023@gmail.com>
2022-01-22 21:11:58 -0700

    word art, functions for budget, deletes, update manager, view employees by

--- config/db/index.js
+++ config/db/index.js
@@ -20,7 +20,7 @@ class DB {
         return this.connection.query('SELECT role.id, title, salary, department.name FROM role LEFT JOIN department ON role.department_id = department.id');
     };
     deleteRole(payload) {
-        return this.connection.query('DELETE FROM role WHERE id =?', payload);
+        return this.connection.query('DELETE FROM role WHERE ?', payload);
     }
     addEmployee(payload) {
         return this.connection.query('INSERT INTO employee SET ?', payload);
@@ -29,13 +29,33 @@ class DB {
         return this.connection.query('SELECT employee.id, CONCAT (employee.first_name, " ", employee.last_name) AS employee, department.name AS department, CONCAT (manager.first_name, " ", manager.last_name) AS manager, role.title FROM employee LEFT JOIN role ON employee.role_id=role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id');
     };
     deleteEmployee(payload) {
-        return this.connection.query('DELETE FROM employee WHERE id =?', payload);
+        return this.connection.query('DELETE FROM employee WHERE ?', payload);
     }
     updateEmployeeNewRole(payload) {
         let id = payload.id;
         let data = {role_id: payload.role_id};
         return this.connection.query(`UPDATE employee SET ? WHERE id='`+id+`'`, data);
      }
+     updateManager(payload) {
+        let id = payload.id;
+        let data = {manager_id: payload.manager_id};
+        return this.connection.query(`UPDATE employee SET ? WHERE id='`+id+`'`, data);
+     }
+     viewByManager(payload) {
+        let id = payload.manager_id;
+        console.log(id);
+        return this.connection.query('SELECT employee.id, CONCAT (employee.first_name, " ", employee.last_name) AS employee, department.name AS department, CONCAT (manager.first_name, " ", manager.last_name) AS manager, role.title FROM employee LEFT JOIN role ON employee.role_id=role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id WHERE employee.manager_id = ?', id);
+    };
+    viewByDepartment(payload) {
+        let id = payload.id;
+        console.log(id);
+        return this.connection.query('SELECT employee.id, CONCAT (employee.first_name, " ", employee.last_name) AS employee, department.name AS department, CONCAT (manager.first_name, " ", manager.last_name) AS manager, role.title FROM employee LEFT JOIN role ON employee.role_id=role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id WHERE department.id = ?', id);
+    };
+    budget(payload) {
+        let id = payload.id;
+        console.log(id);
+        return this.connection.query('SELECT SUM(salary) as TotalBudget FROM employee LEFT JOIN role ON employee.role_id=role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id WHERE department.id = ?', id);
+    };
 };
 
 module.exports = new DB(connection)
--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+console.table(
+`
+ _____       ___  ___   _____   _       _____  __    __  _____   _____            ___  ___       ___   __   _       ___   _____   _____   _____   
+| ____|     /   |/   | |  _  \\ | |     /  _  \\ \\ \\  / / | ____| | ____|          /   |/   |     /   | |  \\ | |     /   | /  ___| | ____| |  _  \\  
+| |__      / /|   /| | | |_| | | |     | | | |  \\ \\/ /  | |__   | |__           / /|   /| |    / /| | |   \\| |    / /| | | |     | |__   | |_| |  
+|  __|    / / |__/ | | |  ___/ | |     | | | |   \\  /   |  __|  |  __|         / / |__/ | |   / / | | | |\\   |   / / | | | |  _  |  __|  |  _  /  
+| |___   / /       | | | |     | |___  | |_| |   / /    | |___  | |___        / /       | |  / /  | | | | \\  |  / /  | | | |_| | | |___  | | \\ \\  
+|_____| /_/        |_| |_|     |_____| \\_____/  /_/     |_____| |_____|      /_/        |_| /_/   |_| |_|  \\_| /_/   |_| \\_____/ |_____| |_|  \\_\\ 
+
+`);
+
 const db = require('./config/db');
 const inquirer = require('inquirer');
 
@@ -13,11 +24,17 @@ const menu = () => {
                     "View all departments",
                     "View all roles",
                     "View all employees",
+                    "View employees by manager",
+                    "View employees by department",
                     "Add a department",
                     "Add a role",
                     "Add an employee",
                     "Update an employee role",
-                    "Delete Department"
+                    "Update manager",
+                    "Delete Department",
+                    "Delete employee role",
+                    "Delete employee",
+                    "Total budget by department"
                 ]
             }])
         .then((response) => {
@@ -31,6 +48,12 @@ const menu = () => {
                 case 'View all employees':
                     viewAllEmployees()
                     break;
+                case 'View employees by manager':
+                    viewByManager()
+                    break;
+                case 'View employees by department':
+                    viewByDepartment()
+                    break;
                 case 'Add a department':
                     addDepartment()
                     break;
@@ -43,10 +66,24 @@ const menu = () => {
                 case 'Update an employee role':
                     updateEmployeeRole()
                     break;
-                case 'Delete Department':
+                case 'Update manager':
+                    updateManager()
+                    break;
+                case 'Delete department':
                     deleteDepartment()
                     break;
+                case 'Delete employee role':
+                    deleteEmployeeRole()
+                    break;
+                case 'Delete employee':
+                    deleteAnEmployee()
+                    break;
+                case 'Total budget by department':
+                        budget()
+                        break;   
                 default:
+                    console.log("invalid choice");
+                    menu();
                     break;
             }
         });
@@ -84,6 +121,8 @@ const viewAllRoles = async () => {
     menu()
 };
 
+// viewAllRoles()
+
 const deleteDepartment = async () => {
     let departments = [];
     let deptTable = await db.viewDept();
@@ -103,7 +142,6 @@ const deleteDepartment = async () => {
     //db.connection.end()
     menu()
 }
-// viewAllRoles()
 
 const addRole = async () => {
     let departments = [];
@@ -140,7 +178,7 @@ const updateEmployeeRole = async () => {
     let roles = [];
     let roleTable = await db.viewRole();
     for (let i = 0; i < roleTable.length; i++) {
-        roles.push({ name: roleTable[i].name, value: roleTable[i].id });
+        roles.push({ name: roleTable[i].title, value: roleTable[i].id });
     }
     let employeeList = [];
     let employeesTable = await db.viewEmployees();
@@ -164,9 +202,32 @@ const updateEmployeeRole = async () => {
         },
 
     ])
-    console.log(employee)
+    //console.log(employee)
     await db.updateEmployeeNewRole(employee);
-    //console.log('Employee role has been updated');
+    console.log('Employee role has been updated');
+    //db.connection.end()
+    menu()
+}
+
+const deleteEmployeeRole = async () => {
+    let roles = [];
+    console.log("Is it stuck?")
+    let roleTable = await db.viewRole();
+    console.table(roleTable);
+    for (let i = 0; i < roleTable.length; i++) {
+        roles.push({ name: roleTable[i].title, value: roleTable[i].id });
+    }
+    console.log(roles);
+    const role = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'id',
+            message: 'Which role would you like to remove?',
+            choices: roles,
+        }
+    ])
+    await db.deleteRole(role);
+    console.log('Role has been deleted.');
     //db.connection.end()
     menu()
 }
@@ -221,9 +282,138 @@ const viewAllEmployees = async () => {
 };
 // viewAllEmployees()
 
+const deleteAnEmployee = async () => {
+    let employees = [];
+    let employeeTable = await db.viewEmployees();
+    for (let i = 0; i < employeeTable.length; i++) {
+        employees.push({ name: employeeTable[i].employee, value: employeeTable[i].id });
+    }
+    const employee = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'id',
+            message: 'Which employee would you like to remove?',
+            choices: employees
+        }
+    ])
+    await db.deleteEmployee(employee);
+    console.log('Employee has been removed.');
+    //db.connection.end()
+    menu()
+}
 
 
+const updateManager = async () => {
+    let employeeList = [];
+    let employeesTable = await db.viewEmployees();
+    for (let i = 0; i < employeesTable.length; i++) {
+        //console.log(employeesTable[i])
+        employeeList.push({ name: employeesTable[i].employee, value: employeesTable[i].id });
+    }
+    //console.log(employeeList);
+    const employee = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'id',
+            message: 'Choose an employee?',
+            choices: employeeList,
+        },
+        {
+            type: 'list',
+            name: 'manager_id',
+            message: 'Who is the employees new manager?',
+            choices: employeeList
+        },
+
+    ])
+    //console.log(employee)
+    await db.updateManager(employee);
+    console.log('Employee role has been updated');
+    //db.connection.end()
+    menu()
+}
+
+const viewByManager = async () => {
+    let employeeList = [];
+    let employeesTable = await db.viewEmployees();
+    for (let i = 0; i < employeesTable.length; i++) {
+        //console.log(employeesTable[i])
+        employeeList.push({ name: employeesTable[i].employee, value: employeesTable[i].id });
+    }
+    //console.log(employeeList);
+    const employee = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'manager_id',
+            message: 'Select a manager?',
+            choices: employeeList
+        },
+
+    ])
+    //console.log(employee)
+    let data = await db.viewByManager(employee);
+    if (data.length == 0) {
+        console.log ('This manager has no emploees!')
+    } else    
+    console.table(data);
+
+    //db.connection.end()
+    menu()
+}
+
+const viewByDepartment = async () => {
+    let departmentList = [];
+    let departmentTable = await db.viewDept();
+    for (let i = 0; i < departmentTable.length; i++) {
+        //console.log(employeesTable[i])
+        departmentList.push({ name: departmentTable[i].name, value: departmentTable[i].id });
+    }
+    //console.log(employeeList);
+    const department = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'id',
+            message: 'Select a department?',
+            choices: departmentList
+        },
+
+    ])
+    //console.log(employee)
+    let data = await db.viewByDepartment(department);
+    if (data.length == 0) {
+        console.log ('This department has no employees!')
+    } else    
+    console.table(data);
+
+    //db.connection.end()
+    menu()
+}
+
+const budget = async () => {
+    let departmentList = [];
+    let departmentTable = await db.viewDept();
+    for (let i = 0; i < departmentTable.length; i++) {
+        //console.log(employeesTable[i])
+        departmentList.push({ name: departmentTable[i].name, value: departmentTable[i].id });
+    }
+    //console.log(employeeList);
+    const department = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'id',
+            message: 'Select a department?',
+            choices: departmentList
+        },
+
+    ])
+    //console.log(employee)
+    let data = await db.budget(department);
+    if (data.length == 0) {
+        console.log ('This department has no employees, therefore no budget!')
+    } else    
+    console.table(data);
 
 
-
-
+    //db.connection.end()
+    menu()
+}
